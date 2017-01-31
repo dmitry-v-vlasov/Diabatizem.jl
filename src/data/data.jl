8def38687297eaa1ae5ba23b41df12cbe75091cd
Dmitry Vlasov <dmitry.v.vlasov@gmail.com>
2017-02-01 01:45:16 +0300

    Interpolation small fix

--- src/data/data.jl
+++ src/data/data.jl
@@ -18,13 +18,14 @@ end
 
 function buildHₐ!(table_Hₐ::DataFrame, data::Data)
   X = convert(Array{Float64}, table_Hₐ[1])
+  ΔR = X[2] - X[1]
   N = numberOfChannels(table_Hₐ)
   data.hamiltonian = Array{Function, 2}(N, N)
   for i = 1:N, j = 1:N
     if i == j
       Y = convert(Array{Float64}, table_Hₐ[i + 1])
-      itp = interpolate(Y, BSpline(Cubic(Flat())), OnGrid())
-      setindex!(data.hamiltonian, R -> itp[R], i, j)
+      itp = interpolate(Y, BSpline(Quadratic(Flat())), OnGrid())
+      setindex!(data.hamiltonian, R -> itp[R/ΔR + 1], i, j)
     else
       setindex!(data.hamiltonian, R -> 0, i, j)
     end
@@ -33,14 +34,15 @@ end
 
 function build_d_dR!(table_∂_∂R::DataFrame, data::Data, N::Int)
   X = convert(Array{Float64}, table_∂_∂R[1])
+  ΔR = X[2] - X[1]
   Nc = size(X, 1) - 1
   data.∂_∂R = Array{Function, 2}(N, N)
   for i = 1:N, j = 1:N
     if i ≠ j
       l = dataColumnOfSymetricMatrix(i, j, N)
       Y = convert(Array{Float64}, table_∂_∂R[l + 1])
-      itp = interpolate(i > j ? Y : -Y, BSpline(Cubic(Flat())), OnGrid())
-      setindex!(data.∂_∂R, R -> itp[R], i, j)
+      itp = interpolate(i < j ? Y : -Y, BSpline(Quadratic(Flat())), OnGrid())
+      setindex!(data.∂_∂R, R -> itp[R/ΔR + 1], i, j)
     else
       setindex!(data.∂_∂R, R -> 0, i, j)
     end
