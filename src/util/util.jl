00edff2185c0e2f7db393399c596b7373ecc3e5a
Dmitry Vlasov <dmitry.v.vlasov@gmail.com>
2018-06-06 00:46:44 +0300

    Current State

--- src/util/util.jl
+++ src/util/util.jl
@@ -65,6 +65,19 @@ function save_data(data::DataFrame, file::AbstractString; header=true)
   writetable(file, data; separator=' ', quotemark=' ', header=header, nastring="EMPTY")
 end
 
+function load_data(file::AbstractString; header=true)
+  readtable(
+    file,
+    header = header, separator = ' ',
+    allowcomments = true, commentmark = '#',
+    skipblanks = true, encoding = :utf8, normalizenames = false
+  )
+end
+
+function save_data(data::DataFrame, file::AbstractString; header=true)
+  writetable(file, data; separator=' ', quotemark=' ', header=header, nastring="EMPTY")
+end
+
 # ---------------------------------
 # Miscellaneous Types
 # ---------------------------------
